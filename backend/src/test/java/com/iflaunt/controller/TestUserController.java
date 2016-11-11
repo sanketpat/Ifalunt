079c021e4bba335c4c9240e5414565d22238ca5f
Sanket Patil <sanket.patil30@gmail.com>
2016-11-11 15:18:44 -0500

    Added junit for photo upload controller/TestUserController.java updated update controller/TestUserController.java added test image file

--- backend/src/test/java/com/iflaunt/controller/TestUserController.java
+++ backend/src/test/java/com/iflaunt/controller/TestUserController.java
@@ -13,17 +13,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.io.File;
+import java.io.FileInputStream;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -45,23 +50,20 @@ public class TestUserController {
     private User user;
 
     @Before
-    public void setup() {
+    public void setup() throws Exception {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
                 .build();
-    }
-    @Test
-	public void testLoginUserController() throws Exception {
-    	
-    	user= new User();
+        user= new User();
     	user.setUserName("spati56l265@uncc.edu");
     	user.setPassword("125634");
-    	
     	Gson gson = new Gson();
     	String json=gson.toJson(user);
     	
     	mockMvc.perform(post("/user/register").header("Content-Type", "application/json").
     			 content(json)).andExpect(status().isOk());   
-    
+    }
+    @Test
+	public void testLoginUserController() throws Exception {
     	
     	mockMvc.perform(post("/user/login").header("Content-Type", "application/json").
    			 content("{\"username\":\"spati56l265@uncc.edu\",\"password\":\"125634\"}")).andExpect(status().isOk());   
@@ -99,8 +101,22 @@ public class TestUserController {
     
     @Test
    	public void testImageUploadUserController() throws Exception {
+       		File f = new File("./src/test/java/com/iflaunt/controller/test.jpg");
+            System.out.println(f.isFile()+"  "+f.getName()+f.exists());
+            FileInputStream fi1 = new FileInputStream(f);
+            MockMultipartFile fstmp = new MockMultipartFile("upload", f.getName(), "multipart/form-data",fi1);
+            MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+            mockMvc.perform(MockMvcRequestBuilders.fileUpload("/user/upload")                
+                    .file(fstmp)
+                    .param("username",user.getUserName()))               
+                    .andExpect(status().isOk());
        	
-       	mockMvc.perform(post("/user/upload").header("", "").
-       			 content("\"name\":\"abc")).andExpect(status().isOk());
+       	
+       	
+       /*	mockMvc.perform(post("/user/upload").header("", "").
+       			 content("\"name\":\"abc")).andExpect(status().isOk());*/
        }
+    
+    
+    
 }
