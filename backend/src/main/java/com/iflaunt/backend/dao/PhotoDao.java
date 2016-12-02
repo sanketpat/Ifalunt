720bae6ebf0ce1b18b9f5937428f9a9bb9b7185e
Muraligowtham <muraligowthamsb@gmail.com>
2016-12-02 17:40:25 -0500

    trends correction

--- backend/src/main/java/com/iflaunt/backend/dao/PhotoDao.java
+++ backend/src/main/java/com/iflaunt/backend/dao/PhotoDao.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.iflaunt.backend.model.User;
@@ -20,6 +21,6 @@ public interface PhotoDao extends CrudRepository<Photo, Long>{
 	Photo findByPhotoId(Long photoId);
 	
 	List<Photo> findAll();
-	@Query("select brand from Photo p group by brand order by count(p) desc")
-	List<Photo> countsBybrand();
+@Query("select brand from Photo p where p.user in(select u.userName from User u where u.city in(select uu.city from User uu where uu.userName=:Userparam)) group by p.brand order by count(p) desc") 
+	List<Photo> countsBybrand(@Param("Userparam") String User);
 }
